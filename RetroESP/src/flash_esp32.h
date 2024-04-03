3f7d2d7c6b3a0af807e80b56bbda3df1556209cf
David <dj.dorant@student.avans.nl>
2024-04-03 15:08:05 +0200

    added highscore and score tracking functionalities, required additional includements of flash_esp classes

---
+++ RetroESP/src/flash_esp32.h
@@ -0,0 +1,60 @@
+
+#pragma once
+#include <zephyr/kernel.h>
+#include <zephyr/sys/reboot.h>
+#include <zephyr/device.h>
+#include <string.h>
+#include <zephyr/drivers/flash.h>
+#include <zephyr/storage/flash_map.h>
+#include <zephyr/fs/nvs.h>
+#include <cstdint>
+#include <sstream>
+#include <cstdint>
+#include <vector>
+#include <string>
+#include <cstring>
+
+
+
+#define PARTITION	storage_partition
+
+#define PARTITION_OFFSET	FIXED_PARTITION_OFFSET(PARTITION)
+#define PARTITION_DEVICE	FIXED_PARTITION_DEVICE(PARTITION)
+
+#define FLASH_PAGE_SIZE   4096
+#define DATA_WORD_0  0x1122
+#define DATA_WORD_1  0xaabb
+#define DATA_WORD_2  0xabcd
+#define DATA_WORD_3  0x1234
+
+#define FLASH_OFFSET2 0x41234
+#define FLASH_PAGE_IDX 37
+
+
+class Flash_esp 
+{
+public:
+     Flash_esp();
+    ~Flash_esp();
+public: 
+    void write_esp_flash();
+    void write_string(std::string data);
+    void erase_esp_flash();
+    std::string read_esp_flash();
+
+    size_t get_size_flash();
+private:
+    uint32_t buf_word = 0U;
+    uint32_t offset;
+    size_t string_size;
+    std::vector<uint32_t> uint32Array;
+    std::vector<uint32_t> uint32Array_read;
+    int count_ASCII =0;
+
+ private: 
+    std::string received_string;
+
+private:
+    std::string uint_array_to_string(const uint32_t* uint_array, uint32_t length);
+    void convert_string_uint_32(size_t size,std::string data );
+};
---
+++ RetroESP/src/scores.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <string.h>
+#include <cstring> // for strstr
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <zephyr/storage/flash_map.h>
+#include <zephyr/drivers/flash.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/device.h>
+#include <zephyr/kernel.h>
+#include <sstream>
+#include "flash_esp32.h"
+
+
+
+class Score
+{
+public:
+    Score(Flash_esp* flash_esp);
+   ~Score();
+public:
+    void assign_boss_points();
+    void assign_monster_points();
+    void assign_time_points();
+     //add transistion for reseting the multiplier when it is done
+    void decrease_multiplier(int time_sim);
+public:
+    void get_leaderboard();
+    void reset_leaderboard();
+    void write_leaderboard();
+    void compare_leaderboard();
+private:
+    int time_multiplier = 100;
+    int monster_exp = 1000;  // base points without multiplier
+    int boss_exp    = 10000; // base points without multiplier
+private:
+    int time = 0;
+    int second_decrease = 35; 
+    int current_score = 1099999900; // gamescore
+    bool high_score = false;
+private:   
+    std::string leader_board;
+    int compare_score;
+    char *player_and_score[10]; // Array to store the split strings
+    char *store_new_score;
+    int replace_score =0;
+    void move_down_leaderboard();
+private:
+    Flash_esp *flash_esp;
+
+
+};
