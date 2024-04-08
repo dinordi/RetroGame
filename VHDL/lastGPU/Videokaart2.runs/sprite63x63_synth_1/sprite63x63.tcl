44b4329e32273b844da7f674e6c3c7fa1873b901
benT034 <ba.tucker@student.avans.nl>
2024-04-08 22:44:29 +0200

    added last version gpu

--- VHDL/lastGPU/Videokaart2.runs/sprite63x63_synth_1/sprite63x63.tcl
+++ VHDL/lastGPU/Videokaart2.runs/sprite63x63_synth_1/sprite63x63.tcl
@@ -4,7 +4,7 @@
 
 set TIME_start [clock seconds] 
 namespace eval ::optrace {
-  variable script "C:/github/RetroGame/VHDL/lastGPU/Videokaart2.runs/sprite63x63_synth_1/sprite63x63.tcl"
+  variable script "C:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.runs/sprite63x63_synth_1/sprite63x63.tcl"
   variable category "vivado_synth"
 }
 
@@ -70,6 +70,7 @@ proc create_report { reportName command } {
   }
 }
 OPTRACE "sprite63x63_synth_1" START { ROLLUP_AUTO }
+set_param xicom.use_bs_reader 1
 set_param project.vivado.isBlockSynthRun true
 set_msg_config -msgmgr_mode ooc_run
 OPTRACE "Creating in-memory project" START { }
@@ -79,19 +80,19 @@ set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
 set_msg_config -source 4 -id {IP_Flow 19-2162} -severity warning -new_severity info
-set_property webtalk.parent_dir C:/github/RetroGame/VHDL/lastGPU/Videokaart2.cache/wt [current_project]
-set_property parent.project_path C:/github/RetroGame/VHDL/lastGPU/Videokaart2.xpr [current_project]
+set_property webtalk.parent_dir C:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.cache/wt [current_project]
+set_property parent.project_path C:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.xpr [current_project]
 set_property XPM_LIBRARIES {XPM_CDC XPM_MEMORY} [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
-set_property board_part_repo_paths {C:/Users/Joey/AppData/Roaming/Xilinx/Vivado/2023.2/xhub/board_store/xilinx_board_store} [current_project]
+set_property board_part_repo_paths {C:/Users/bentu/AppData/Roaming/Xilinx/Vivado/2023.2/xhub/board_store/xilinx_board_store} [current_project]
 set_property board_part digilentinc.com:basys3:part0:1.2 [current_project]
-set_property ip_output_repo c:/github/RetroGame/VHDL/lastGPU/Videokaart2.cache/ip [current_project]
+set_property ip_output_repo c:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 OPTRACE "Creating in-memory project" END { }
 OPTRACE "Adding files" START { }
-read_ip -quiet C:/github/RetroGame/VHDL/lastGPU/Videokaart2.srcs/sources_1/ip/sprite63x63/sprite63x63.xci
-set_property used_in_implementation false [get_files -all c:/github/RetroGame/VHDL/lastGPU/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_ooc.xdc]
+read_ip -quiet C:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.srcs/sources_1/ip/sprite63x63/sprite63x63.xci
+set_property used_in_implementation false [get_files -all c:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_ooc.xdc]
 
 OPTRACE "Adding files" END { }
 # Mark all dcp files as not used in implementation to prevent them from being
@@ -107,7 +108,7 @@ set_property used_in_implementation false [get_files dont_touch.xdc]
 set_param ips.enableIPCacheLiteLoad 1
 OPTRACE "Configure IP Cache" START { }
 
-set cacheID [config_ip_cache -export -no_bom  -dir C:/github/RetroGame/VHDL/lastGPU/Videokaart2.runs/sprite63x63_synth_1 -new_name sprite63x63 -ip [get_ips sprite63x63]]
+set cacheID [config_ip_cache -export -no_bom  -dir C:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.runs/sprite63x63_synth_1 -new_name sprite63x63 -ip [get_ips sprite63x63]]
 
 OPTRACE "Configure IP Cache" END { }
 if { $cacheID == "" } {
@@ -162,32 +163,32 @@ create_report "sprite63x63_synth_1_synth_report_utilization_0" "report_utilizati
 OPTRACE "synth reports" END { }
 
 if { [catch {
-  file copy -force C:/github/RetroGame/VHDL/lastGPU/Videokaart2.runs/sprite63x63_synth_1/sprite63x63.dcp c:/github/RetroGame/VHDL/lastGPU/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63.dcp
+  file copy -force C:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.runs/sprite63x63_synth_1/sprite63x63.dcp c:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63.dcp
 } _RESULT ] } { 
   send_msg_id runtcl-3 status "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
   error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
 }
 
 if { [catch {
-  write_verilog -force -mode synth_stub c:/github/RetroGame/VHDL/lastGPU/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_stub.v
+  write_verilog -force -mode synth_stub c:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_stub.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a Verilog synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_vhdl -force -mode synth_stub c:/github/RetroGame/VHDL/lastGPU/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_stub.vhdl
+  write_vhdl -force -mode synth_stub c:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_stub.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a VHDL synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_verilog -force -mode funcsim c:/github/RetroGame/VHDL/lastGPU/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_sim_netlist.v
+  write_verilog -force -mode funcsim c:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_sim_netlist.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the Verilog functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_vhdl -force -mode funcsim c:/github/RetroGame/VHDL/lastGPU/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_sim_netlist.vhdl
+  write_vhdl -force -mode funcsim c:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_sim_netlist.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the VHDL functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
@@ -197,32 +198,32 @@ if { [catch {
 
 
 if { [catch {
-  file copy -force C:/github/RetroGame/VHDL/lastGPU/Videokaart2.runs/sprite63x63_synth_1/sprite63x63.dcp c:/github/RetroGame/VHDL/lastGPU/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63.dcp
+  file copy -force C:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.runs/sprite63x63_synth_1/sprite63x63.dcp c:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63.dcp
 } _RESULT ] } { 
   send_msg_id runtcl-3 status "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
   error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
 }
 
 if { [catch {
-  file rename -force C:/github/RetroGame/VHDL/lastGPU/Videokaart2.runs/sprite63x63_synth_1/sprite63x63_stub.v c:/github/RetroGame/VHDL/lastGPU/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_stub.v
+  file rename -force C:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.runs/sprite63x63_synth_1/sprite63x63_stub.v c:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_stub.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a Verilog synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/github/RetroGame/VHDL/lastGPU/Videokaart2.runs/sprite63x63_synth_1/sprite63x63_stub.vhdl c:/github/RetroGame/VHDL/lastGPU/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_stub.vhdl
+  file rename -force C:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.runs/sprite63x63_synth_1/sprite63x63_stub.vhdl c:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_stub.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a VHDL synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/github/RetroGame/VHDL/lastGPU/Videokaart2.runs/sprite63x63_synth_1/sprite63x63_sim_netlist.v c:/github/RetroGame/VHDL/lastGPU/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_sim_netlist.v
+  file rename -force C:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.runs/sprite63x63_synth_1/sprite63x63_sim_netlist.v c:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_sim_netlist.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the Verilog functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/github/RetroGame/VHDL/lastGPU/Videokaart2.runs/sprite63x63_synth_1/sprite63x63_sim_netlist.vhdl c:/github/RetroGame/VHDL/lastGPU/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_sim_netlist.vhdl
+  file rename -force C:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.runs/sprite63x63_synth_1/sprite63x63_sim_netlist.vhdl c:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_sim_netlist.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the VHDL functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
@@ -230,15 +231,15 @@ if { [catch {
 close [open .end.used_ip_cache.rst w]
 }; # end if cacheID 
 
-if {[file isdir C:/github/RetroGame/VHDL/lastGPU/Videokaart2.ip_user_files/ip/sprite63x63]} {
+if {[file isdir C:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.ip_user_files/ip/sprite63x63]} {
   catch { 
-    file copy -force c:/github/RetroGame/VHDL/lastGPU/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_stub.v C:/github/RetroGame/VHDL/lastGPU/Videokaart2.ip_user_files/ip/sprite63x63
+    file copy -force c:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_stub.v C:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.ip_user_files/ip/sprite63x63
   }
 }
 
-if {[file isdir C:/github/RetroGame/VHDL/lastGPU/Videokaart2.ip_user_files/ip/sprite63x63]} {
+if {[file isdir C:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.ip_user_files/ip/sprite63x63]} {
   catch { 
-    file copy -force c:/github/RetroGame/VHDL/lastGPU/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_stub.vhdl C:/github/RetroGame/VHDL/lastGPU/Videokaart2.ip_user_files/ip/sprite63x63
+    file copy -force c:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.gen/sources_1/ip/sprite63x63/sprite63x63_stub.vhdl C:/Users/bentu/Downloads/Videokaart2.cache/Videokaart2.ip_user_files/ip/sprite63x63
   }
 }
 file delete __synthesis_is_running__
