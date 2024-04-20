8a19f91ba120c5f7ff54a34efe95d5bac3b6dee1
Joey van Noort <joey@lvannoort.com>
2024-04-20 18:06:51 +0200

    Added doxygen for fpga class

--- RetroESP/src/fpga.h
+++ RetroESP/src/fpga.h
@@ -1,3 +1,8 @@
+/**
+ * @file fpga.h
+ * @brief This file contains the FPGA class and related data structures for communication with the Basys3 FPGA.
+ */
+
 #pragma once
 
 #include <zephyr/kernel.h>
@@ -6,26 +11,34 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/sys/printk.h>
 
-
-#include <string>
-#include <thread>
-#include <chrono>
-
 #define SPIBB_NODE	DT_NODELABEL(spi2)
-#define arrSize 900
 
-//SPI and FPGA handler
+/**
+ * @class FPGA
+ * @brief Class representing an FPGA device.
+ *
+ * This class provides methods for interacting with the Basys3 FPGA device.
+ */
 class FPGA
 {
 public:
+    /**
+     * @brief Construct a new FPGA object.
+     */
     FPGA();
+
+    /**
+     * @brief Destroy the FPGA object.
+     */
     ~FPGA();
+
+    /**
+     * @brief Send sprite data to the FPGA.
+     * @param buf1 Buffer containing the sprite data.
+     * @param spriteDataCount Number of sprite data elements in the buffer.
+     */
     void sendSprite(uint16_t* buf1, int spriteDataCount);
 
 private:
-    struct spi_cs_control cs_ctrl;
-    // const struct device *const dev;
-    // uint16_t *buffID;
-    // uint16_t *buffX;
-    // uint16_t *buffY;
+    struct spi_cs_control cs_ctrl; /**< SPI chip select control structure. */
 };
